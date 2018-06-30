89f471054b6f5ae7acb994e74858b238e95d0581
Srija S <srijakumar7@gmail.com>
2018-06-30 21:51:24 +0000

    automatically backed up by learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -1,3 +1,10 @@
+require 'nokogiri'
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+end
+
 describe "#create_project_hash" do
 
   it "can be called on with no errors" do
