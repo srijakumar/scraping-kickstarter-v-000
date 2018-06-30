8f4753339897dd15bb738fd5e39a4738012685ab
Srija S <srijakumar7@gmail.com>
2018-06-30 22:17:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ def create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
 project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
   end
 
